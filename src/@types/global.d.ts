b901b0cd5fd62f05ac1727455f1fbff3106039eb
Yuki Ito <yukiito@uw.edu>
2020-05-03 19:53:31 -0400

    use ff type def

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,23 +1,3 @@
 // Replaced during Webpack build process
 declare var BROWSER: 'chrome' | 'firefox';
 declare var EXTENSION_VERSION: string;
-
-interface Window {
-  // Used for firefox only APIs
-  browser: typeof browser;
-}
-
-type browser = typeof chrome;
-
-declare namespace browser.commands {
-  export type Command = chrome.commands.Command;
-
-  export type CommandEvent = chrome.commands.CommandEvent;
-
-  export var getAll: typeof chrome.commands.getAll;
-
-  export var onCommand: CommandEvent;
-
-  //Firefox only
-  export function update(details: Command): Promise<void>;
-}
